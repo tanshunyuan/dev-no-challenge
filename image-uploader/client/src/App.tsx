a37bfe281e6249881c526ba6941554b7ab68e4c8
shun yuan <tanshunyuan99@gmail.com>
2022-05-16 20:21:45 +0800

    feat: added drag n drop

--- image-uploader/client/src/App.tsx
+++ image-uploader/client/src/App.tsx
@@ -1,24 +1,52 @@
-import { useState } from 'react'
-import './styles/app.css'
+import React, { useState, ChangeEvent, useCallback } from "react";
+import Dropzone, { FileWithPath, useDropzone } from "react-dropzone";
+import "./styles/app.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [image, setImage] = useState("");
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    uploadImage(acceptedFiles[0]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    maxFiles: 1,
+  });
+  const handleImage = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files !== null) {
+      const file: File = files[0];
+      uploadImage(file);
+    }
+  };
+  const uploadImage = (imageFile: File) => {
+    console.log(imageFile);
+  };
 
   return (
     <div className="container">
-    <div className="image-container">
-    <div className="image-header">
-    <h4>Upload your image</h4>
-    <p>File should be Jpeg, Png...</p>
+      <div className="image-container">
+        <div className="image-header">
+          <h4>Upload your image</h4>
+          <p>File should be Jpeg, Png...</p>
+        </div>
+        <div {...getRootProps({ className: "image-body" })}>
+          <input {...getInputProps()} />
+          <p>Drag and drop your image here</p>
+        </div>
+        <p className="image-divider">Or</p>
+        <div className="image-action">
+          <input
+            type="file"
+            className="btn bg-primary"
+            id="file-upload"
+            name="img"
+            accept="image/*"
+            onChange={handleImage}
+          />
+        </div>
+      </div>
     </div>
-    <div className="image-body"></div>
-    <p className="image-divider">Or</p>
-    <div className="image-action">
-    <button className="btn bg-primary">Choose a File</button>
-    </div>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
