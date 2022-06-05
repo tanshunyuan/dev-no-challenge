63b238cea994976832f341009a771abc66386cee
shun yuan <tanshunyuan99@gmail.com>
2022-06-05 17:58:36 +0800

    chore: cannot rmb

--- image-uploader/backend/index.js
+++ image-uploader/backend/index.js
@@ -2,18 +2,21 @@ const express = require("express");
 const dotenv = require("dotenv");
 const multer = require("multer");
 const ImageKit = require("imagekit");
+const cors = require('cors');
+
 const uploadFile = multer({ storage: multer.memoryStorage() }).single(
   "imageFile"
 );
 
 dotenv.config();
-
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors())
 
 app.post("/upload", uploadFile, async (req, res) => {
+  console.log(req)
   const file = req.file;
   const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
@@ -34,7 +37,7 @@ app.post("/upload", uploadFile, async (req, res) => {
       console.log("ERROR => ", err);
       return err;
     });
-  res.send("Express + TypeScript Server");
+  res.status(200).send(result.url);
 });
 
 app.listen(port, () => {
